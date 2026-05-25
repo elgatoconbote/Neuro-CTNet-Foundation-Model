18585a85d5043b3b1afefd1216b0dc9f87a63cab
elgatoconbote <elgatoconbote79@gmail.com>
2026-05-25 23:07:05 +0200

    Add modular tensor utilities API

---
+++ src/nctnet/tensors.py
@@ -0,0 +1,11 @@
+from .core import split_up, merge_up, rms, spos, center, sinusoidal_slot_basis, ring_laplacian
+
+__all__ = [
+    "split_up",
+    "merge_up",
+    "rms",
+    "spos",
+    "center",
+    "sinusoidal_slot_basis",
+    "ring_laplacian",
+]
